0b279be76d04cdf228ded232d591e815e3d3e0c6
6631501108-Sasikran <6631501108@lamduan.mfu.ac.th>
2025-08-21 15:35:23 +0700

    Complete getting input

--- dart_git/bin/dart_git.dart
+++ dart_git/bin/dart_git.dart
@@ -1,8 +1,24 @@
-
+import 'dart:io';
 void main() {
   List expenses = [
     {"id": 1, "item": "lunch", "paid": 50},
     {"id": 2, "item": "coffee", "paid": 70},
   ];
-
+  print('===== Expense tracking app ===== ');
+  print('1. Show all');
+  print('2. Total paid');
+  stdout.write('Enter 1 or 2....');
+  int? choice = int.tryParse(stdin.readLineSync()!);
+  
+  if (choice != null) {
+    if (choice == 1 ){
+      //show all
+      print(choice);
+    } else if (choice == 2) {
+      // total expenses
+      print(choice);
+    }
+  } else {
+    print('Wrong input');
+  }
 }
